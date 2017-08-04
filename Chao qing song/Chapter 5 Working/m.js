6ce5a71e40f826ee49cee6cde1d536eb3b0f22f5
littleflute <13699175164@sina.cn>
2017-08-04 13:00:50 -0500

    Add files via upload

---
+++ Chao qing song/Chapter 5 Working/m.js
@@ -0,0 +1,17 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "C05-Part 1 Vocabulary.mp3";		
+f[2]	= "C05-Part 3 Useful Expressions-01.mp3";		
+f[3]	= "C05-Part 3 Useful Expressions-02.mp3";		
+f[4]	= "C05-Part 3 Useful Expressions-03.mp3";		
+f[5]	= "C05-Part 3 Useful Expressions-04.mp3";		
+f[6]	= "C05-Part 3 Useful Expressions-05.mp3";		
+f[7]	= "C05-Part 3 Useful Expressions-06.mp3";		
+f[8]	= "C05-Part 3 Useful Expressions-07.mp3";		
+f[9]	= "C05-Part 3 Useful Expressions-08.mp3";		
+f[10]	= "C05-Part 3 Useful Expressions-09.mp3";		
+f[11]	= "C05-Part 3 Useful Expressions-10.mp3";		
+f[12]	= "C05-Part 4 Conversations-01.mp3";		
+f[13]	= "C05-Part 4 Conversations-02.mp3";		
+f[14]	= "C05-Part 4 Conversations-03.mp3";		
+f[15]	= "C05-Part 4 Conversations-04.mp3";		
