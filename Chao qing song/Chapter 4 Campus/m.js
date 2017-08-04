77143ce27e5192fc6658deb783af36f93afa395c
littleflute <13699175164@sina.cn>
2017-08-04 12:58:41 -0500

    Add files via upload

---
+++ Chao qing song/Chapter 4 Campus/m.js
@@ -0,0 +1,15 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "C04-Part 1 Vocabulary.mp3";		
+f[2]	= "C04-Part 3 Useful Expressions-01.mp3";		
+f[3]	= "C04-Part 3 Useful Expressions-02.mp3";		
+f[4]	= "C04-Part 3 Useful Expressions-03.mp3";		
+f[5]	= "C04-Part 3 Useful Expressions-04.mp3";		
+f[6]	= "C04-Part 3 Useful Expressions-05.mp3";		
+f[7]	= "C04-Part 3 Useful Expressions-06.mp3";		
+f[8]	= "C04-Part 3 Useful Expressions-07.mp3";		
+f[9]	= "C04-Part 3 Useful Expressions-08.mp3";		
+f[10]	= "C04-Part 4 Conversations-01.mp3";		
+f[11]	= "C04-Part 4 Conversations-02.mp3";		
+f[12]	= "C04-Part 4 Conversations-03.mp3";		
+f[13]	= "C04-Part 4 Conversations-04.mp3";		
