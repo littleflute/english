0cf95b6d07f9986e55d41be6ca27ff2a85c2c4b3
littleflute <13699175164@sina.cn>
2017-08-04 13:03:30 -0500

    Add files via upload

---
+++ Chao qing song/Chapter 7 Communication/m.js
@@ -0,0 +1,16 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "C07-Part 1 Vocabulary.mp3";		
+f[2]	= "C07-Part 3 Useful Expressions-01.mp3";		
+f[3]	= "C07-Part 3 Useful Expressions-02.mp3";		
+f[4]	= "C07-Part 3 Useful Expressions-03.mp3";		
+f[5]	= "C07-Part 3 Useful Expressions-04.mp3";		
+f[6]	= "C07-Part 3 Useful Expressions-05.mp3";		
+f[7]	= "C07-Part 3 Useful Expressions-06.mp3";		
+f[8]	= "C07-Part 3 Useful Expressions-07.mp3";		
+f[9]	= "C07-Part 3 Useful Expressions-08.mp3";		
+f[10]	= "C07-Part 3 Useful Expressions-09.mp3";		
+f[11]	= "C07-Part 4 Conversations-01.mp3";		
+f[12]	= "C07-Part 4 Conversations-02.mp3";		
+f[13]	= "C07-Part 4 Conversations-03.mp3";		
+f[14]	= "C07-Part 4 Conversations-04.mp3";		
