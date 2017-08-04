5e06d8eb995e2df019a9fcc2031ae769bed84c7b
littleflute <13699175164@sina.cn>
2017-08-04 13:06:21 -0500

    Add files via upload

---
+++ Chao qing song/Chapter 10 Love/m.js
@@ -0,0 +1,17 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "C10-Part 1 Vocabulary.mp3";		
+f[2]	= "C10-Part 3 Useful Expressions-01.mp3";		
+f[3]	= "C10-Part 3 Useful Expressions-02.mp3";		
+f[4]	= "C10-Part 3 Useful Expressions-03.mp3";		
+f[5]	= "C10-Part 3 Useful Expressions-04.mp3";		
+f[6]	= "C10-Part 3 Useful Expressions-05.mp3";		
+f[7]	= "C10-Part 3 Useful Expressions-06.mp3";		
+f[8]	= "C10-Part 3 Useful Expressions-07.mp3";		
+f[9]	= "C10-Part 3 Useful Expressions-08.mp3";		
+f[10]	= "C10-Part 3 Useful Expressions-09.mp3";		
+f[11]	= "C10-Part 3 Useful Expressions-10.mp3";		
+f[12]	= "C10-Part 4 Conversations-01.mp3";		
+f[13]	= "C10-Part 4 Conversations-02.mp3";		
+f[14]	= "C10-Part 4 Conversations-03.mp3";		
+f[15]	= "C10-Part 4 Conversations-04.mp3";		
