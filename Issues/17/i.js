9f439682f3eb761ee59f524d1321e67a84304308
littleflute <13699175164@sina.cn>
2018-03-13 00:19:45 -0700

    Update i.js

--- Issues/17/i.js
+++ Issues/17/i.js
@@ -1,20 +1,35 @@
-
  var d = bl$("blrIssue17Div");
 d.mv = blo0.blMDiv(d,d.id+"mv","issue17",300,100,500,400,blGrey[0]);
 d.mv.v1 = blo0.blDiv(d.mv,d.mv.id + "v1","v1",blGrey[1]);
 function _jobClass(){
-  this.v = "v0.0. 11";
+  this.v = "v0.0. 41";
+  this.blrIssue14= function(b,d){	
+      if(!d.v){ 
+         d.v =blo0.blDiv(d,d.id + "v","#14",blGrey[1]);
+         var _f = function (o){
+            eval(o[2].body);     
+            api3("https://api.github.com/repos/littleflute/english/issues/14"); 
+         }
+         w3.getHttpObject("https://api.github.com/repos/littleflute/JavaScript/issues/7/comments", _f);	
+      }
+      _on_off_div(b,d);
+  }
   this.blrAboutPlx= function(b,d){		
 		var s = "blrAboutPlx<br>"; 
 		s += "<a target='_blank' href='https://github.com/littleflute/english/edit/master/Issues/17/i.js'"
 		s += " style='color:blue;'";
 		s +=">";
-		s += " i17*  ";
+		s += " i17.js*  ";
 		s += "</a>"; 
 		s += "<a target='_blank'  href='https://littleflute.github.io/english/Issues/17/i.js'";
 		s += " style='color:yellow;'";
 		s +=">";
-		s += " i17 ";
+		s += " i17.js ";
+		s += "</a>"; 
+		s += "<a target='_blank'  href='https://github.com/littleflute/english/issues/17'";
+		s += " style='color:yellow;'";
+		s +=">";
+		s += " #17 ";
 		s += "</a>"; 
 		d.innerHTML = s;
 		_on_off_div(b,d);
