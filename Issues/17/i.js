3e7e8057c0f7df82ddff66ce076ac9ed5e968cd2
littleflute <13699175164@sina.cn>
2018-03-13 09:27:04 -0700

    Update i.js

--- Issues/17/i.js
+++ Issues/17/i.js
@@ -2,7 +2,39 @@
 d.mv = blo0.blMDiv(d,d.id+"mv","issue17",300,100,500,400,blGrey[0]);
 d.mv.v1 = blo0.blDiv(d.mv,d.mv.id + "v1","v1",blGrey[1]);
 function _jobClass(){
-  this.v = "v0.0. 41";
+  this.v = "v0.0. 44";
+  this.blrIssue17C1= function(b,d){	
+      if(!d.v){ 
+         d.v =blo0.blDiv(d,d.id + "v","17C1",blGrey[1]);
+         var _f = function (o){
+            eval(o[0].body);     
+         }
+         w3.getHttpObject("https://api.github.com/repos/littleflute/english/issues/17/comments", _f);	
+      }
+      _on_off_div(b,d);
+  }
+  this.blrIssue19= function(b,d){	
+      if(!d.v){ 
+         d.v =blo0.blDiv(d,d.id + "v","#19",blGrey[1]);
+         var _f = function (o){
+            eval(o[3].body);     
+            _blVPlayer(); 
+         }
+         w3.getHttpObject("https://api.github.com/repos/littleflute/JavaScript/issues/7/comments", _f);	
+      }
+      _on_off_div(b,d);
+  }
+  this.blrIssue18= function(b,d){	
+      if(!d.v){ 
+         d.v =blo0.blDiv(d,d.id + "v","#18",blGrey[1]);
+         var _f = function (o){
+            eval(o[2].body);     
+            api3("https://api.github.com/repos/littleflute/english/issues/18"); 
+         }
+         w3.getHttpObject("https://api.github.com/repos/littleflute/JavaScript/issues/7/comments", _f);	
+      }
+      _on_off_div(b,d);
+  }
   this.blrIssue14= function(b,d){	
       if(!d.v){ 
          d.v =blo0.blDiv(d,d.id + "v","#14",blGrey[1]);
