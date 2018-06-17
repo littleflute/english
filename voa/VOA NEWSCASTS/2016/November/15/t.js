682424afd0997150bddadd1efd3c0ee14b5b811a
littleflute <13699175164@sina.cn>
2018-06-17 00:08:22 -0700

    Update t.js

--- voa/VOA NEWSCASTS/2016/November/15/t.js
+++ voa/VOA NEWSCASTS/2016/November/15/t.js
@@ -1,6 +1,7 @@
 var v1 = bl$("id1234v1");
 v1.tb = blo0.blDiv(v1, v1.id + "tb", "tb",blColor[5]);
 v1.tb.b1 = blo0.blBtn(v1.tb, v1.tb.id + "b1","b1",blColor[0]);
+v1.tb.b2 = blo0.blBtn(v1.tb, v1.tb.id + "b2","b2",blColor[2]);
 
 function _add_time_Btn(d, t, s){
 	if(!d.n){d.n = 0;}
@@ -21,6 +22,13 @@ v1.tb.b1.onclick = function(){
 	_add_time_Btn(v1,2,"2000ms");
 	_add_time_Btn(v1,5,"5000ms");
 	_add_time_Btn(v1,10,"10000ms");
-	_add_time_Btn(v1,11,"11000ms");
-	_add_time_Btn(v1,20,"20s");
+}
+
+v1.dScript= blo0.blDiv(v1, v1.id + "dScript", "dScript", blColor[5]);
+v1._2do = function(txt){
+	alert(txt);
+}
+v1.tb.b2.onclick  = function(){ 
+	//alert(v1._2do);
+	blo0.blAjx (v1,"http://www.beautifullover.org/readme.txt");	
 }
