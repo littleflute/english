695e1a9c90dbf5ab8506fecfb77d274d1b4974d3
littleflute <13699175164@sina.cn>
2018-09-14 13:53:36 -0700

    Add files via upload

---
+++ voa/SCIENCE & TECHNOLOGY/2018/september/13/t.js
@@ -0,0 +1,92 @@
+var d = bl$("id1234");
+d.v2 = blo0.blDiv(d,d.id + "v2", "v2_t.js: v0.0. 11 ",blGrey[1]);
+d.v2.b1 = blo0.blBtn(d.v2,d.v2.id + "b1", "txt",blColor[1]);
+d.v2.b1.onclick = function(){
+    	d.v2. divTxt = blo0.blDiv(d.v2,d.v2.id + "divTxt", "divTxt : v0.0.2 ",blGrey[2]);
+	d.v2. divTxt.innerHTML = _txt;
+}
+
+var _txt = function(){
+
+/*
+
+Scientists say they have discovered three new species of fish in one of the deepest places on Earth.
+
+All three species were found in the Pacific Ocean, off the coast of Chile and Peru. They were deep in the Atacama Trench -- 7,500 meters below the surface.
+
+Researchers from Britain¡¯s Newcastle University say the new species have been given temporary names based on their coloring. They are known as ¡®the pink, the blue and the purple Atacama Snailfish.¡¯
+
+The three snailfish species are different from most other fish. Their unusual bodies are built to deal with the cold water and extreme pressure deep in the ocean.
+
+All three are small and translucent, meaning light can pass through their skin. They also have no scales on the skin for protection.
+
+
+
+
+
+
+
+
+
+
+
+
+From left, purple, pink, and blue Atacama snailfish. (Newscastle University)
+
+
+Thomas Linley is with Newscastle University. He said in a statement, ¡°the hardest structures in their bodies are the bones in their inner ear which give them balance and their teeth. Without the extreme pressure and cold to support their bodies they are extremely fragile and melt rapidly when brought to the surface.¡±
+
+A team of 40 scientists from 17 nations captured video of the fish in their natural environment.
+
+Other scientists say the ¡°blue¡± Atacama Snailfish appeared to be a rare ¡°winged¡± snailfish. But it has long fin and big head similar to the Ethereal Snailfish off the coast of Mariana Islands.
+
+The ¡°pink¡± was closer in appearance to the Mariana Snailfish, a species found in the deepest part of Mariana Trench on the other side of the Pacific.
+
+The third species is a small purple snailfish. It is similar to fish living at the depth of 3,500 meters. One of the purple snailfish, about 9 centimeters long, was brought to the surface for further study.
+
+
+
+-v1-
+
+In addition to the discovery of the new species of snailfish, scientists also captured rare video of long-legged isopods, known as Munnopsids. The shellfish has a small body, about the size of a human hand. It can swim backwards and upside down using its extremely long legs.
+
+The scientists say what they have found in the deep sea is an environment rich with many species in large numbers.
+
+From the video shot by the Newcastle scientists, Linley said, ¡°There is something about the snailfish that allows them to adapt to living very deep. Beyond the reach of other fish they are free of competitors and predators.¡±
+
+He added, ¡°they seem to be quite active and look very well-fed.¡±
+
+I'm Anna Matteo.
+
+
+
+Hai Do adapted this story for Learning English based on The Conversation. George Grow was the editor.
+
+Write to us in the Comments Section or on our Facebook page.
+
+_____________________________________________________________
+
+Words in This Story
+
+
+species - n. a group of plants or animals that are similar and can produce young plants or animals
+
+translucent - adj. not completely clear, like transparent, but clear enough to allow light to pass through
+
+scale - n. one of many small thin plates that cover the bodies of fish or snakes
+
+teeth - n. plural of tooth, hard white objects inside the mouth that are used for biting and chewing
+
+fragile - adj. easily broken or damaged
+
+winged - adj. having wings
+
+fin - n. a thin flat part that sticks out from the body of a fish and is used for moving through water
+
+adapt - v. to change so that it functions better or is better suited for a purpose
+
+predator - n. an animal that lives by killing and eating other animals
+
+*/
+
+}
