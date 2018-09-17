a95c3c0c96a45e74df7c6b9705b106e63f2d28dc
littleflute <13699175164@sina.cn>
2018-09-17 13:54:35 -0700

    Add files via upload

---
+++ voa/AS IT IS/2018/september/16/t.js
@@ -0,0 +1,4 @@
+var s = '<a href= "https://mp.weixin.qq.com/s/MPiWbau7qrUGkBxJG3OFHA" target="_blank">as423</a> ';
+
+var divP = bl$("id1234");
+var divT = blo0.blDiv(divP, divP.id + "divT", s, blGrey[0]);
