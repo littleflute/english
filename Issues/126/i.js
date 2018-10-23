41a4ee77ac67db32151fe175791c5abe00a0bb5f
littleflute <13699175164@sina.cn>
2018-10-23 10:34:57 -0700

    Update i.js

--- Issues/126/i.js
+++ Issues/126/i.js
@@ -12,9 +12,15 @@ s +=">";		s += " i62.js*  ";		s += "</a> ";
  s += '</div></div>';
 d1.innerHTML = s;
 
- function _comments(o) {	
-	 for(i in o){
-                    var d = blo0.blDiv( d1, d1.id + i, o[i].body, blGrey[0]); 
+ function _comments(o) {
+         var d = blo0.blDiv( d1, d1.id + "_v",  "v", blGrey[0]); 	
+        d.v = blo0.blDiv(d,d.id+"v","v",blGrey[2]);
+        d.v1 = blo0.blDiv(d,d.id+"v1","v1",blGrey[3]);
+	 for(i in o){ //o[i].body
+                    var _b = blo0.blBtn(d.v , d.v.id + "_btn_" + i, i, blColor[i]);    _b.v = d.v1;
+                    _b.onclick = function(_this, _txt){
+                         return function(){ _this.v.innerHTML = _txt; eval(_txt);}
+                     }(_b, o[i].body);  
         }
 }
 w3.getHttpObject("https://api.github.com/repos/littleflute/english/issues/126/comments", _comments);	
